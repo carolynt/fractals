566f18ad9c97debedc6beae83a9722aaf363ec32
carolynt <carolynton1@gmail.com>
2015-07-28 01:55:56 +0000

    heroku

--- application_controller.rb
+++ application_controller.rb
@@ -6,8 +6,22 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-  get '/create.erb' do
+  get '/create' do
     erb :create
   end
-
+ get '/whats_a_fractal' do
+   erb :whats_a_fractal
+  end
+  get '/square_fractal' do
+    erb :square_fractal
+  end
+  get '/examples' do
+    erb :examples
+  end
+  get '/fractal_tree' do
+    erb :fractal_tree
+  end
+  get '/dragon_curve' do
+    erb :dragon_curve
+  end
 end
